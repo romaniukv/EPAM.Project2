e1b28776c9abce3a63954dc282a9310ad6f48387
romaniukV <romaniukv255@gmail.com>
2018-04-13 19:21:30 +0300

    Add regex constants.

---
+++ src/com/epam/project2/utils/RegExUtils.java
@@ -0,0 +1,7 @@
+package com.epam.project2.utils;
+
+public class RegExUtils {
+
+    public static final String MENU_KEY_REGEX = "^[12]$";
+    public static final String LETTER_REGEX = "^[a-zA-Z]$";
+}
