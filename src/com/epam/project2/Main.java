ee23ae5d98fe54cb8575c5411d4e922e4c64081f
romaniukV <romaniukv255@gmail.com>
2018-04-13 19:28:09 +0300

    Add Main.

--- src/com/epam/project2/Main.java
+++ src/com/epam/project2/Main.java
@@ -1,14 +1,15 @@
 package com.epam.project2;
 
+import com.epam.project2.controller.Runner;
 import com.epam.project2.model.TextTasks;
 import com.epam.project2.model.entities.Letter;
 import com.epam.project2.model.entities.Text;
 import com.epam.project2.utils.InputUtils;
+import com.epam.project2.view.Viewer;
 
 public class Main {
 
     public static void main(String[] args) {
-        TextTasks textTasks = new TextTasks(new Text(InputUtils.inputText("Input text:")), new Letter('g'));
-        textTasks.sortByLetterCount();
+        new Runner(new TextTasks(), new Viewer()).run();
     }
 }
