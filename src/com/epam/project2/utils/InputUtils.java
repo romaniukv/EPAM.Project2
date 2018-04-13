9940b33bc4aa363544e928373c5ff752ed256db7
romaniukV <romaniukv255@gmail.com>
2018-04-13 19:27:09 +0300

    Updated InputUtils.

--- src/com/epam/project2/utils/InputUtils.java
+++ src/com/epam/project2/utils/InputUtils.java
@@ -1,14 +1,34 @@
 package com.epam.project2.utils;
 
+import com.epam.project2.view.Viewer;
+
 import java.util.Scanner;
 
 public class InputUtils {
 
     private static Scanner in = new Scanner(System.in);
 
-    public static String inputText(String msg) {
-        System.out.println(msg);
-        String text = in.nextLine();
-        return text;
+    public static int inputMenuKey() {
+        while (true) {
+            String str = in.nextLine();
+            if (str.matches(RegExUtils.MENU_KEY_REGEX))
+                return Integer.valueOf(str);
+            else
+                System.out.println(Viewer.INPUT_ERROR);
+        }
+    }
+
+    public static String inputText() {
+        return in.nextLine();
+    }
+
+    public static char inputLetter() {
+        while (true) {
+            String str = in.nextLine();
+            if (str.matches(RegExUtils.LETTER_REGEX))
+                return str.charAt(0);
+            else
+                System.out.println(Viewer.INPUT_ERROR);
+        }
     }
 }
