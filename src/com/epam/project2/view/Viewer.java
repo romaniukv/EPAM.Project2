92fb66628ada0bc41381366fc7ef4bbfbdf469c9
romaniukV <romaniukv255@gmail.com>
2018-04-13 19:27:41 +0300

    Fix MENU constant.

--- src/com/epam/project2/view/Viewer.java
+++ src/com/epam/project2/view/Viewer.java
@@ -4,7 +4,7 @@ public class Viewer {
 
     public static final String MENU = "**************Меню*********" +
             "1. Отсортировать слова в тексте по убыванию количества вхождений\n" +
-            "заданного символа, а в случае равенства – по алфавиту." +
+            "заданного символа, а в случае равенства – по алфавиту.\n" +
             "2. Выход.";
     public static final String INPUT_TEXT = "Введите текст:";
     public static final String INPUT_LETTER = "Введите символ:";
