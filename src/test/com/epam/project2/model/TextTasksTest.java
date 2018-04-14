a24f201c2ea6af27ebbfde3ee79115b204b5742b
romaniukV <romaniukv255@gmail.com>
2018-04-14 15:35:22 +0300

    Finish TextTasks tests.

--- src/test/com/epam/project2/model/TextTasksTest.java
+++ src/test/com/epam/project2/model/TextTasksTest.java
@@ -11,21 +11,21 @@ import static org.junit.Assert.*;
 public class TextTasksTest {
 
     private Text testText;
-    private Letter leter;
+    private Letter letter;
     private String expectedResult;
 
     @Before
     public void setUp(){
-        testText = new Text("");
-        leter = new Letter('a');
-        expectedResult = "";
+        testText = new Text("How brave they’ll all think me at home! Why, I wouldn’t say anything about it, even if I fell off the top of the house (which was very likely true).");
+        letter = new Letter('e');
+        expectedResult = "even brave fell home house likely me the the they’ll true very about all anything at how i i if it of off say think top was which why wouldn’t";
     }
 
     @Test
     public void sortByLetterCount() {
         TextTasks textTasks = new TextTasks();
         textTasks.setText(testText);
-        textTasks.setLetter(leter);
+        textTasks.setLetter(letter);
         String actualResult = textTasks.sortByLetterCount();
         assertEquals(expectedResult, actualResult);
     }
